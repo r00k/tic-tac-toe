12e507d3f580fdebae09977a84313846c0a732b1
Ben Orenstein <ben.orenstein@gmail.com>
2014-01-11 15:02:12 -0500

    Add autorun plugin for tests. Update info.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject tic-tac-toe "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Tic Tac Toe"
+  :url "https://github.com/r00k/tic-tac-toe"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[quickie "0.2.4"]]
   :dependencies [[org.clojure/clojure "1.5.1"]])
